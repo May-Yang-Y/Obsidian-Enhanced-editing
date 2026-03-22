6a2609d2e672b217e864671fc27cbd5785678737
RuYi Yuan <24041212517@stu.xidian.edu.cn>
2026-03-22 14:51:21 +0800

    Simplify highlight markup in editor

--- main.js
+++ main.js
@@ -28,6 +28,7 @@ const DEFAULT_SETTINGS = {
     version: "0.4.5",
     hColor: "",
     bColor: "",
+    highlightStylePreset: "soft-fill",
     hColor1: "#F36208",
     hColor2: "#81B300",
     hColor3: "#2485E3",
@@ -40,6 +41,45 @@ const DEFAULT_SETTINGS = {
     bColor4: "#F0A7D8",
     bColor5: "#ADEFEF"
 };
+
+const 高亮样式预设 = {
+    "soft-fill": {
+        名称: "柔和底色",
+        描述: "轻微圆角与留白，更接近舒服耐看的荧光笔。",
+        样式: (color) => `background:${color}; padding:0 0.18em; border-radius:0.22em; box-decoration-break:clone; -webkit-box-decoration-break:clone;`
+    },
+    "rounded-tag": {
+        名称: "圆角标签",
+        描述: "更像标签卡片，适合强调重点术语。",
+        样式: (color) => `background:${color}; padding:0.08em 0.38em; border-radius:999px; box-decoration-break:clone; -webkit-box-decoration-break:clone;`
+    },
+    "marker-line": {
+        名称: "记号笔",
+        描述: "保留原生高亮感觉，但边缘更柔和，阅读更轻松。",
+        样式: (color) => `background:linear-gradient(180deg, transparent 16%, ${color} 16%, ${color} 88%, transparent 88%); padding:0 0.08em; box-decoration-break:clone; -webkit-box-decoration-break:clone;`
+    },
+    "soft-underline": {
+        名称: "下划荧光",
+        描述: "只在文字底部形成柔和高亮，适合长文标注。",
+        样式: (color) => `background:linear-gradient(180deg, transparent 0, transparent 62%, ${color} 62%, ${color} 100%); padding:0 0.08em 0.02em; border-radius:0.16em; box-decoration-break:clone; -webkit-box-decoration-break:clone;`
+    }
+};
+
+function 获取高亮样式(styleKey, color) {
+    const 当前预设 = 高亮样式预设[styleKey] || 高亮样式预设["soft-fill"];
+    return 当前预设.样式(color);
+}
+
+function 获取高亮预览HTML(styleKey, color, text = "高亮预览") {
+    return `<span style="${获取高亮样式(styleKey, color)}">${text}</span>`;
+}
+
+function 获取背景颜色槽位(settings, color) {
+    const 当前颜色 = (color || "").toLowerCase();
+    const 颜色列表 = [settings.bColor1, settings.bColor2, settings.bColor3, settings.bColor4, settings.bColor5]
+        .map((_color) => (_color || "").toLowerCase());
+    return 颜色列表.indexOf(当前颜色) + 1;
+}
 
 var 简体字表 = "皑蔼碍爱肮翱袄奥坝罢摆败颁办绊帮绑镑谤剥饱宝报鲍辈贝钡狈备惫绷笔毕毙币闭边编贬变辩辫标鳖别瘪濒滨宾摈饼并拨钵铂驳卜补财参蚕残惭惨灿苍舱仓沧厕侧册测层诧搀掺蝉馋谗缠铲产阐颤场尝长偿肠厂畅钞车彻尘沉陈衬撑称惩诚骋痴迟驰耻齿炽冲虫宠畴踌筹绸丑橱厨锄雏础储触处传疮闯创锤纯绰辞词赐聪葱囱从丛凑蹿窜错达带贷担单郸掸胆惮诞弹当挡党荡档捣岛祷导盗灯邓敌涤递缔颠点垫电淀凋钓调迭谍叠钉顶锭订丢东动栋冻斗犊独读赌镀锻断缎兑队对吨顿钝夺堕鹅额讹恶饿儿尔饵贰发罚阀珐矾钒烦范贩饭访纺飞诽废费纷坟奋愤粪丰枫锋风疯冯缝讽凤肤辐抚辅赋复负讣妇缚该钙盖干杆赶秆赣冈刚钢纲岗皋镐搁鸽阁铬个给龚宫巩贡钩沟苟构购够蛊顾剐挂关观馆惯贯广规硅归龟闺轨诡柜贵刽辊滚锅国过骇韩汉号阂鹤贺横轰鸿红后壶护沪户哗华画划话怀坏欢环还缓换唤痪焕涣黄谎挥辉毁贿秽会烩汇讳诲绘荤浑伙获货祸击机积饥迹讥鸡绩缉极辑级挤几蓟剂济计记际继纪夹荚颊贾钾价驾歼监坚笺间艰缄茧检碱硷拣捡简俭减荐槛鉴践贱见键舰剑饯渐溅涧将浆蒋桨奖讲酱胶浇骄娇搅铰矫侥脚饺缴绞轿较秸阶节茎鲸惊经颈静镜径痉竞净纠厩旧驹举据锯惧剧鹃绢杰洁结诫届紧锦仅谨进晋烬尽劲荆觉决诀绝钧军骏开凯颗壳课垦恳抠库裤夸块侩宽矿旷况亏岿窥馈溃扩阔蜡腊莱来赖蓝栏拦篮阑兰澜谰揽览懒缆烂滥琅捞劳涝乐镭垒类泪篱狸离里鲤礼丽厉励砾历沥隶俩联莲连镰怜涟帘敛脸链恋炼练粮凉两辆谅疗辽镣猎临邻鳞凛赁龄铃凌灵岭领馏刘龙聋咙笼垄拢陇楼娄搂篓芦卢颅庐炉掳卤虏鲁赂禄录陆驴吕铝侣屡缕虑滤绿峦挛孪滦乱抡轮伦仑沦纶论萝罗逻锣箩骡骆络妈玛码蚂马骂吗买麦卖迈脉瞒馒蛮满谩猫锚铆贸么霉没镁门闷们锰梦眯谜弥觅幂绵缅庙灭悯闽鸣铭谬谋亩呐钠纳难挠脑恼闹馁内拟你腻撵捻酿鸟聂啮镊镍柠狞宁拧泞钮纽脓浓农疟诺欧鸥殴呕沤盘庞抛赔喷鹏骗飘频贫苹凭评泼颇扑铺朴谱栖凄脐齐骑岂启气弃讫牵扦钎铅迁签谦钱钳潜浅谴堑枪呛墙蔷强抢锹桥乔侨翘窍窃钦亲寝轻氢倾顷请庆琼穷趋区躯驱龋颧权劝却鹊确让饶扰绕热韧认纫荣绒软锐闰润洒萨鳃赛叁伞丧骚扫涩杀刹纱筛晒删闪陕赡缮墒伤赏烧绍赊摄慑设绅审婶肾渗声绳胜圣师狮湿诗尸时蚀实识驶势适释饰视试寿兽枢输书赎属术树竖数帅双谁税顺说硕烁丝饲耸怂颂讼诵擞苏诉肃虽随绥岁孙损笋缩琐锁獭挞抬台态摊贪瘫滩坛谭谈叹汤烫涛绦讨腾誊锑题体屉条贴铁厅听烃铜统头秃图涂团颓蜕脱鸵驮驼椭洼袜弯湾顽万网韦违围为潍维苇伟伪纬喂谓卫温闻纹稳问瓮挝蜗涡窝卧呜钨乌污诬无芜吴坞雾务误锡牺袭习铣戏细虾辖峡侠狭厦吓锨鲜纤咸贤衔闲显险现献县馅羡宪线厢镶乡详响项萧嚣销晓啸蝎协挟携胁谐写泻谢锌衅兴凶汹锈绣虚嘘须许叙绪续轩悬选癣绚学勋询寻驯训讯逊压鸦鸭哑亚讶阉烟盐严岩颜阎艳厌砚彦谚验鸯杨扬疡阳痒养样瑶摇尧遥窑谣药爷页业叶一医铱颐遗仪彝蚁艺亿忆义诣议谊译异绎荫阴银饮隐樱婴鹰应缨莹萤营荧蝇赢颖哟拥佣痈踊咏涌优忧邮铀犹游诱于舆鱼渔娱与屿语吁御狱誉预驭鸳渊辕园员圆缘远愿约跃钥岳粤悦阅云郧匀陨运蕴酝晕韵杂灾载攒暂赞赃脏凿枣灶责择则泽贼赠扎札轧铡闸栅诈斋债毡盏斩辗崭栈战绽张涨帐账胀赵蛰辙锗这贞针侦诊镇阵挣睁狰争帧症郑证织职执纸志挚掷帜质滞钟终种肿众诌轴皱昼骤猪诸诛烛瞩嘱贮铸筑注驻专砖转赚桩庄装妆壮状锥赘坠缀谆准着浊兹资渍踪综总纵邹诅组钻锕嗳嫒瑷暧霭谙铵鹌媪骜鳌钯呗钣鸨龅鹎贲锛荜哔滗铋筚跸苄缏笾骠飑飙镖镳鳔傧缤槟殡膑镔髌鬓禀饽钹鹁钸骖黪恻锸侪钗冁谄谶蒇忏婵骣觇禅镡伥苌怅阊鲳砗伧谌榇碜龀枨柽铖铛饬鸱铳俦帱雠刍绌蹰钏怆缍鹑辍龊鹚苁骢枞辏撺锉鹾哒鞑骀绐殚赕瘅箪谠砀裆焘镫籴诋谛绨觌镝巅钿癫铫鲷鲽铤铥岽鸫窦渎椟牍笃黩簖怼镦炖趸铎谔垩阏轭锇锷鹗颚颛鳄诶迩铒鸸鲕钫鲂绯镄鲱偾沣凫驸绂绋赙麸鲋鳆钆赅尴擀绀戆睾诰缟锆纥镉颍亘赓绠鲠诟缑觏诂毂钴锢鸪鹄鹘鸹掴诖掼鹳鳏犷匦刿妫桧鲑鳜衮绲鲧埚呙帼椁蝈铪阚绗颉灏颢诃阖蛎黉讧荭闳鲎浒鹕骅桦铧奂缳锾鲩鳇诙荟哕浍缋珲晖诨馄阍钬镬讦诘荠叽哜骥玑觊齑矶羁虿跻霁鲚鲫郏浃铗镓蛲谏缣戋戬睑鹣笕鲣鞯绛缰挢峤鹪鲛疖颌鲒卺荩馑缙赆觐刭泾迳弪胫靓阄鸠鹫讵屦榉飓钜锔窭龃锩镌隽谲珏皲剀垲忾恺铠锴龛闶钪铐骒缂轲钶锞颔龈铿喾郐哙脍狯髋诓诳邝圹纩贶匮蒉愦聩篑阃锟鲲蛴崃徕涞濑赉睐铼癞籁岚榄斓镧褴阆锒唠崂铑铹痨鳓诔缧俪郦坜苈莅蓠呖逦骊缡枥栎轹砺锂鹂疠粝跞雳鲡鳢蔹奁潋琏殓裢裣鲢魉缭钌鹩蔺廪檩辚躏绫棂蛏鲮浏骝绺镏鹨茏泷珑栊胧砻偻蒌喽嵝镂瘘耧蝼髅垆撸噜闾泸渌栌橹轳辂辘氇胪鸬鹭舻鲈脔娈栾鸾銮囵荦猡泺椤脶镙榈褛锊呒唛嬷杩劢缦镘颡鳗麽扪焖懑钔芈谧猕祢渑腼黾缈缪闵缗谟蓦馍殁镆钼铙讷铌鲵辇鲶茑袅陧蘖嗫颟蹑苎咛聍侬哝驽钕傩讴怄瓯蹒疱辔纰罴铍谝骈缥嫔钋镤镨蕲骐绮桤碛颀颃鳍佥荨悭骞缱椠钤嫱樯戗炝锖锵镪羟跄诮谯荞缲硗跷惬锲箧锓揿鲭茕蛱巯赇虮鳅诎岖阒觑鸲诠绻辁铨阕阙悫荛娆桡饪轫嵘蝾缛铷颦蚬飒毵糁缫啬铯穑铩鲨酾讪姗骟钐鳝垧殇觞厍滠畲诜谂渖谥埘莳弑轼贳铈鲥绶摅纾闩铄厮驷缌锶鸶薮馊飕锼谡稣谇荪狲唢睃闼铊鳎钛鲐昙钽锬顸傥饧铴镗韬铽缇鹈阗粜龆鲦恸钭钍抟饨箨鼍娲腽纨绾辋诿帏闱沩涠玮韪炜鲔阌莴龌邬庑怃妩骛鹉鹜饩阋玺觋硖苋莶藓岘猃娴鹇痫蚝籼跹芗饷骧缃飨哓潇骁绡枭箫亵撷绁缬陉荥馐鸺诩顼谖铉镟谑泶鳕埙浔鲟垭娅桠氩厣赝俨兖谳恹闫酽魇餍鼹炀轺鹞鳐靥谒邺晔烨诒呓峄饴怿驿缢轶贻钇镒镱瘗舣铟瘾茔莺萦蓥撄嘤滢潆璎鹦瘿颏罂镛莸铕鱿伛俣谀谕蓣嵛饫阈妪纡觎欤钰鹆鹬龉橼鸢鼋钺郓芸恽愠纭韫殒氲瓒趱錾驵赜啧帻箦谮缯谵诏钊谪辄鹧浈缜桢轸赈祯鸩诤峥钲铮筝骘栉栀轵轾贽鸷蛳絷踬踯觯锺纣绉伫槠铢啭馔颞骓缒诼镯谘缁辎赀眦锱龇鲻偬诹驺鲰镞缵躜鳟讠谫郄勐凼坂垅垴埯埝苘荬荮莜莼菰藁揸吒吣咔咝咴噘噼嚯幞岙嵴彷徼犸狍馀馇馓馕愣憷懔丬溆滟溷漤潴澹甯纟绔绱珉枧桊桉槔橥轱轷赍肷胨飚煳煅熘愍淼砜磙眍钚钷铘铞锃锍锎锏锘锝锪锫锿镅镎镢镥镩镲稆鹋鹛鹱疬疴痖癯裥襁耢颥螨麴鲅鲆鲇鲞鲴鲺鲼鳊鳋鳘鳙鞒鞴齄";
 var 繁体字表 = "皚藹礙愛骯翺襖奧壩罷擺敗頒辦絆幫綁鎊謗剝飽寶報鮑輩貝鋇狽備憊繃筆畢斃幣閉邊編貶變辯辮標鱉別癟瀕濱賓擯餅並撥缽鉑駁蔔補財參蠶殘慚慘燦蒼艙倉滄廁側冊測層詫攙摻蟬饞讒纏鏟產闡顫場嘗長償腸廠暢鈔車徹塵沈陳襯撐稱懲誠騁癡遲馳恥齒熾沖蟲寵疇躊籌綢醜櫥廚鋤雛礎儲觸處傳瘡闖創錘純綽辭詞賜聰蔥囪從叢湊躥竄錯達帶貸擔單鄲撣膽憚誕彈當擋黨蕩檔搗島禱導盜燈鄧敵滌遞締顛點墊電澱雕釣調叠諜疊釘頂錠訂丟東動棟凍鬥犢獨讀賭鍍鍛斷緞兌隊對噸頓鈍奪墮鵝額訛惡餓兒爾餌貳發罰閥琺礬釩煩範販飯訪紡飛誹廢費紛墳奮憤糞豐楓鋒風瘋馮縫諷鳳膚輻撫輔賦復負訃婦縛該鈣蓋幹桿趕稈贛岡剛鋼綱崗臯鎬擱鴿閣鉻個給龔宮鞏貢鉤溝茍構購夠蠱顧剮掛關觀館慣貫廣規矽歸龜閨軌詭櫃貴劊輥滾鍋國過駭韓漢號閡鶴賀橫轟鴻紅後壺護滬戶嘩華畫劃話懷壞歡環還緩換喚瘓煥渙黃謊揮輝毀賄穢會燴匯諱誨繪葷渾夥獲貨禍擊機積饑跡譏雞績緝極輯級擠幾薊劑濟計記際繼紀夾莢頰賈鉀價駕殲監堅箋間艱緘繭檢堿鹼揀撿簡儉減薦檻鑒踐賤見鍵艦劍餞漸濺澗將漿蔣槳獎講醬膠澆驕嬌攪鉸矯僥腳餃繳絞轎較稭階節莖鯨驚經頸靜鏡徑痙競凈糾廄舊駒舉據鋸懼劇鵑絹傑潔結誡屆緊錦僅謹進晉燼盡勁荊覺決訣絕鈞軍駿開凱顆殼課墾懇摳庫褲誇塊儈寬礦曠況虧巋窺饋潰擴闊蠟臘萊來賴藍欄攔籃闌蘭瀾讕攬覽懶纜爛濫瑯撈勞澇樂鐳壘類淚籬貍離裏鯉禮麗厲勵礫歷瀝隸倆聯蓮連鐮憐漣簾斂臉鏈戀煉練糧涼兩輛諒療遼鐐獵臨鄰鱗凜賃齡鈴淩靈嶺領餾劉龍聾嚨籠壟攏隴樓婁摟簍蘆盧顱廬爐擄鹵虜魯賂祿錄陸驢呂鋁侶屢縷慮濾綠巒攣孿灤亂掄輪倫侖淪綸論蘿羅邏鑼籮騾駱絡媽瑪碼螞馬罵嗎買麥賣邁脈瞞饅蠻滿謾貓錨鉚貿麽黴沒鎂門悶們錳夢瞇謎彌覓冪綿緬廟滅憫閩鳴銘謬謀畝吶鈉納難撓腦惱鬧餒內擬妳膩攆撚釀鳥聶嚙鑷鎳檸獰寧擰濘鈕紐膿濃農瘧諾歐鷗毆嘔漚盤龐拋賠噴鵬騙飄頻貧蘋憑評潑頗撲鋪樸譜棲淒臍齊騎豈啟氣棄訖牽扡釬鉛遷簽謙錢鉗潛淺譴塹槍嗆墻薔強搶鍬橋喬僑翹竅竊欽親寢輕氫傾頃請慶瓊窮趨區軀驅齲顴權勸卻鵲確讓饒擾繞熱韌認紉榮絨軟銳閏潤灑薩鰓賽三傘喪騷掃澀殺剎紗篩曬刪閃陜贍繕墑傷賞燒紹賒攝懾設紳審嬸腎滲聲繩勝聖師獅濕詩屍時蝕實識駛勢適釋飾視試壽獸樞輸書贖屬術樹豎數帥雙誰稅順說碩爍絲飼聳慫頌訟誦擻蘇訴肅雖隨綏歲孫損筍縮瑣鎖獺撻擡臺態攤貪癱灘壇譚談嘆湯燙濤絳討騰謄銻題體屜條貼鐵廳聽烴銅統頭禿圖塗團頹蛻脫鴕馱駝橢窪襪彎灣頑萬網韋違圍為濰維葦偉偽緯餵謂衛溫聞紋穩問甕撾蝸渦窩臥嗚鎢烏汙誣無蕪吳塢霧務誤錫犧襲習銑戲細蝦轄峽俠狹廈嚇鍁鮮纖鹹賢銜閑顯險現獻縣餡羨憲線廂鑲鄉詳響項蕭囂銷曉嘯蠍協挾攜脅諧寫瀉謝鋅釁興兇洶銹繡虛噓須許敘緒續軒懸選癬絢學勛詢尋馴訓訊遜壓鴉鴨啞亞訝閹煙鹽嚴巖顏閻艷厭硯彥諺驗鴦楊揚瘍陽癢養樣瑤搖堯遙窯謠藥爺頁業葉壹醫銥頤遺儀彜蟻藝億憶義詣議誼譯異繹蔭陰銀飲隱櫻嬰鷹應纓瑩螢營熒蠅贏穎喲擁傭癰踴詠湧優憂郵鈾猶遊誘於輿魚漁娛與嶼語籲禦獄譽預馭鴛淵轅園員圓緣遠願約躍鑰嶽粵悅閱雲鄖勻隕運蘊醞暈韻雜災載攢暫贊贓臟鑿棗竈責擇則澤賊贈紮劄軋鍘閘柵詐齋債氈盞斬輾嶄棧戰綻張漲帳賬脹趙蟄轍鍺這貞針偵診鎮陣掙睜猙爭幀癥鄭證織職執紙誌摯擲幟質滯鐘終種腫眾謅軸皺晝驟豬諸誅燭矚囑貯鑄築註駐專磚轉賺樁莊裝妝壯狀錐贅墜綴諄準著濁茲資漬蹤綜總縱鄒詛組鉆錒噯嬡璦曖靄諳銨鵪媼驁鰲鈀唄鈑鴇齙鵯賁錛蓽嗶潷鉍篳蹕芐緶籩驃颮飆鏢鑣鰾儐繽檳殯臏鑌髕鬢稟餑鈸鵓鈽驂黲惻鍤儕釵囅諂讖蕆懺嬋驏覘禪鐔倀萇悵閶鯧硨傖諶櫬磣齔棖檉鋮鐺飭鴟銃儔幬讎芻絀躕釧愴綞鶉輟齪鶿蓯驄樅輳攛銼鹺噠韃駘紿殫賧癉簞讜碭襠燾鐙糴詆諦綈覿鏑巔鈿癲銚鯛鰈鋌銩崠鶇竇瀆櫝牘篤黷籪懟鐓燉躉鐸諤堊閼軛鋨鍔鶚顎顓鱷誒邇鉺鴯鮞鈁魴緋鐨鯡僨灃鳧駙紱紼賻麩鮒鰒釓賅尷搟紺戇睪誥縞鋯紇鎘潁亙賡綆鯁詬緱覯詁轂鈷錮鴣鵠鶻鴰摑詿摜鸛鰥獷匭劌媯檜鮭鱖袞緄鯀堝咼幗槨蟈鉿闞絎頡灝顥訶闔蠣黌訌葒閎鱟滸鶘驊樺鏵奐繯鍰鯇鰉詼薈噦澮繢琿暉諢餛閽鈥鑊訐詰薺嘰嚌驥璣覬齏磯羈蠆躋霽鱭鯽郟浹鋏鎵蟯諫縑戔戩瞼鶼筧鰹韉絳韁撟嶠鷦鮫癤頜鮚巹藎饉縉贐覲剄涇逕弳脛靚鬮鳩鷲詎屨櫸颶鉅鋦窶齟錈鐫雋譎玨皸剴塏愾愷鎧鍇龕閌鈧銬騍緙軻鈳錁頷齦鏗嚳鄶噲膾獪髖誆誑鄺壙纊貺匱蕢憒聵簣閫錕鯤蠐崍徠淶瀨賚睞錸癩籟嵐欖斕鑭襤閬鋃嘮嶗銠鐒癆鰳誄縲儷酈壢藶蒞蘺嚦邐驪縭櫪櫟轢礪鋰鸝癘糲躒靂鱺鱧蘞奩瀲璉殮褳襝鰱魎繚釕鷯藺廩檁轔躪綾欞蟶鯪瀏騮綹鎦鷚蘢瀧瓏櫳朧礱僂蔞嘍嶁鏤瘺耬螻髏壚擼嚕閭瀘淥櫨櫓轤輅轆氌臚鸕鷺艫鱸臠孌欒鸞鑾圇犖玀濼欏腡鏍櫚褸鋝嘸嘜嬤榪勱縵鏝顙鰻麼捫燜懣鍆羋謐獼禰澠靦黽緲繆閔緡謨驀饃歿鏌鉬鐃訥鈮鯢輦鯰蔦裊隉蘗囁顢躡苧嚀聹儂噥駑釹儺謳慪甌蹣皰轡紕羆鈹諞駢縹嬪釙鏷鐠蘄騏綺榿磧頎頏鰭僉蕁慳騫繾槧鈐嬙檣戧熗錆鏘鏹羥蹌誚譙蕎繰磽蹺愜鍥篋鋟撳鯖煢蛺巰賕蟣鰍詘嶇闃覷鴝詮綣輇銓闋闕愨蕘嬈橈飪軔嶸蠑縟銣顰蜆颯毿糝繅嗇銫穡鎩鯊釃訕姍騸釤鱔坰殤觴厙灄畬詵諗瀋謚塒蒔弒軾貰鈰鰣綬攄紓閂鑠廝駟緦鍶鷥藪餿颼鎪謖穌誶蓀猻嗩脧闥鉈鰨鈦鮐曇鉭錟頇儻餳鐋鏜韜鋱緹鵜闐糶齠鰷慟鈄釷摶飩籜鼉媧膃紈綰輞諉幃闈溈潿瑋韙煒鮪閿萵齷鄔廡憮嫵騖鵡鶩餼鬩璽覡硤莧薟蘚峴獫嫻鷴癇蠔秈躚薌餉驤緗饗嘵瀟驍綃梟簫褻擷紲纈陘滎饈鵂詡頊諼鉉鏇謔澩鱈塤潯鱘埡婭椏氬厴贗儼兗讞懨閆釅魘饜鼴煬軺鷂鰩靨謁鄴曄燁詒囈嶧飴懌驛縊軼貽釔鎰鐿瘞艤銦癮塋鶯縈鎣攖嚶瀅瀠瓔鸚癭頦罌鏞蕕銪魷傴俁諛諭蕷崳飫閾嫗紆覦歟鈺鵒鷸齬櫞鳶黿鉞鄆蕓惲慍紜韞殞氳瓚趲鏨駔賾嘖幘簀譖繒譫詔釗謫輒鷓湞縝楨軫賑禎鴆諍崢鉦錚箏騭櫛梔軹輊贄鷙螄縶躓躑觶鍾紂縐佇櫧銖囀饌顳騅縋諑鐲諮緇輜貲眥錙齜鯔傯諏騶鯫鏃纘躦鱒訁譾郤猛氹阪壟堖垵墊檾蕒葤蓧蒓菇槁摣咤唚哢噝噅撅劈謔襆嶴脊仿僥獁麅餘餷饊饢楞怵懍爿漵灩混濫瀦淡寧糸絝緔瑉梘棬案橰櫫軲軤賫膁腖飈糊煆溜湣渺碸滾瞘鈈鉕鋣銱鋥鋶鐦鐧鍩鍀鍃錇鎄鎇鎿鐝鑥鑹鑔穭鶓鶥鸌癧屙瘂臒襇繈耮顬蟎麯鮁鮃鮎鯗鯝鯴鱝鯿鰠鰵鱅鞽韝齇";
@@ -1540,16 +1580,40 @@ class MyPlugin extends obsidian.Plugin {
 
     onunload() {
         console.log('卸载插件');
+        if (this.highlightStyleElement) {
+            this.highlightStyleElement.remove();
+            this.highlightStyleElement = null;
+        }
+    }
+
+    获取高亮样式表文本() {
+        const 颜色列表 = [this.settings.bColor1, this.settings.bColor2, this.settings.bColor3, this.settings.bColor4, this.settings.bColor5];
+        return 颜色列表.map((_color, index) => {
+            return `span.zh-enhanced-highlight.zh-enhanced-highlight-${index + 1}{${获取高亮样式(this.settings.highlightStylePreset, _color)}}`;
+        }).join("\n");
+    }
+
+    更新高亮样式表() {
+        if (!this.settings) {
+            return;
+        }
+        if (!this.highlightStyleElement) {
+            this.highlightStyleElement = document.createElement('style');
+            this.highlightStyleElement.setAttribute('data-enhanced-editing-highlight', 'true');
+            document.head.appendChild(this.highlightStyleElement);
+        }
+        this.highlightStyleElement.textContent = this.获取高亮样式表文本();
     }
 
     async loadSettings() {
         console.log('加载了配置');
         console.log(this.app.customCss.themes);
         this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-     
+        this.更新高亮样式表();
     }
     async saveSettings() {
         await this.saveData(this.settings);
+        this.更新高亮样式表();
     }
 
     /** 以下为基础功能函数 */
@@ -3041,24 +3105,38 @@ class MyPlugin extends obsidian.Plugin {
         this.settings.bColor = _color;
         newNotice = new obsidian.Notice("多彩背景格式刷 已打开！",0);
     }
+    获取背景高亮样式(_color) {
+        return 获取高亮样式(this.settings.highlightStylePreset, _color);
+    }
+
+    获取背景高亮Html(_color, txt) {
+        const 颜色槽位 = 获取背景颜色槽位(this.settings, _color);
+        if (颜色槽位 > 0) {
+            return '\<span class=\"zh-enhanced-highlight zh-enhanced-highlight-'+颜色槽位+'\"\>'+txt+'\<\/span\>';
+        }
+        return '\<span style=\"'+this.获取背景高亮样式(_color)+'\"\>'+txt+'\<\/span\>';
+    }
+
     转换背景颜色() {
         this.获取编辑器信息 ();
         if(所选文本 == ""){return};
-        let _html0 = /\<span style=[\"'][^\<\>]+:[0-9a-zA-Z#]+[\"'][^\<\>]*\>[^\<\>]+\<\/span\>/g;
-        let _html1 = /^\<span style=[\"'][^\<\>]+:[0-9a-zA-Z#]+[\"'][^\<\>]*\>([^\<\>]+)\<\/span\>$/;
-        let _html2 = '\<span style=\"background\:'+this.settings.bColor+'\"\>$1\<\/span\>';
-        let _html3 = /\<span style=[^\<]*$|^[^\>]*span\>/g;	//是否只包含一侧的<>
+        let _html0 = /\<span\s+(?:style|class)=[\"'][^\<\>]*[\"']\>[^\<\>]+\<\/span\>/gi;
+        let _html1 = /^\<span\s+(?:style|class)=([\"'])[^\<\>]*\1\>([^\<\>]+)\<\/span\>$/i;
+        let _html2 = (txt) => this.获取背景高亮Html(this.settings.bColor, txt);
+        let _html3 = /\<span\b[^\>]*$|^[^\>]*\<\/?span\b[^\>]*\>?/i;	//是否只包含一侧的<>
         if (_html3.test(所选文本)){
             return; //new obsidian.Notice("不能转换颜色！");
         }else if (_html0.test(所选文本)){
             if(_html1.test(所选文本)){
-                所选文本 = 所选文本.replace(_html1,_html2); 
+                所选文本 = 所选文本.replace(_html1, function(_all, _quote, _text){
+                    return _html2(_text);
+                }); 
             }else{
-                所选文本 = 所选文本.replace(/\<span style=[\"'][^\<\>]+:[0-9a-zA-Z#]+[\"'][^\<\>]*\>|\<\/span\>/g,"");
+                所选文本 = 所选文本.replace(/\<span\s+(?:style|class)=[\"'][^\<\>]*[\"']\>|\<\/span\>/gi,"");
                 //new obsidian.Notice("需要去除颜色！");
             }
         }else{
-            所选文本 = 所选文本.replace(/^(.+)$/mg,_html2);  //new obsidian.Notice("可以转换颜色！");
+            所选文本 = 所选文本.replace(/^(.+)$/mg,function(_all, _text){ return _html2(_text); });  //new obsidian.Notice("可以转换颜色！");
         }
         this.替换所选文本 (所选文本);
         编辑模式.exec("goRight");
@@ -4479,6 +4557,40 @@ class editSettingsTab extends obsidian.PluginSettingTab {
             .setName('📣 设置彩色背景效果（Html语法）功能')
             .setDesc('点击颜色块调节颜色，在笔记编辑区划选文本后按下「Ctrl+Alt+ 1-5」快捷键，即可转为相应背景颜色的文本。')
 
+        const 背景样式预览 = containerEl.createDiv({ cls: 'enhanced-editing-highlight-preview' });
+        const 渲染背景样式预览 = () => {
+            const 预设 = 高亮样式预设[this.plugin.settings.highlightStylePreset] || 高亮样式预设['soft-fill'];
+            背景样式预览.empty();
+            背景样式预览.createEl('div', { text: `当前样式：${预设.名称}` });
+            背景样式预览.createEl('div', { text: 预设.描述, attr: { style: 'margin: 6px 0 10px; color: var(--text-muted);' } });
+            const 预览行 = 背景样式预览.createDiv({ attr: { style: 'display:flex; flex-wrap:wrap; gap:8px; align-items:center; margin-bottom: 12px;' } });
+            预览行.createDiv({ text: '示例：', attr: { style: 'color: var(--text-muted);' } });
+            [this.plugin.settings.bColor1, this.plugin.settings.bColor2, this.plugin.settings.bColor3].forEach((color, index) => {
+                预览行.createDiv({
+                    attr: { style: 'font-size: 14px; line-height: 1.9;' },
+                    text: ''
+                }).innerHTML = 获取高亮预览HTML(this.plugin.settings.highlightStylePreset, color, `预览 ${index + 1}`);
+            });
+        };
+
+        new obsidian.Setting(containerEl)
+            .setName('高亮背景样式')
+            .setDesc('为所有 Html 背景高亮统一选择一种更柔和的样式；保存时会尽量写成更短的 class 形式，减少编辑时看到的大段 style 代码。')
+            .addDropdown(dropdown => {
+                Object.keys(高亮样式预设).forEach((key) => {
+                    dropdown.addOption(key, 高亮样式预设[key].名称);
+                });
+                dropdown
+                    .setValue(this.plugin.settings.highlightStylePreset)
+                    .onChange(async (value) => {
+                        this.plugin.settings.highlightStylePreset = value;
+                        await this.plugin.saveSettings();
+                        渲染背景样式预览();
+                    });
+            });
+
+        渲染背景样式预览();
+
         const heatmapColourPicker1 = containerEl.createEl("input", {
             type: "color",
         });
@@ -4486,6 +4598,7 @@ class editSettingsTab extends obsidian.PluginSettingTab {
         heatmapColourPicker1.addEventListener("change", async () => {
             this.plugin.settings.bColor1 = heatmapColourPicker1.value;
             await this.plugin.saveSettings();
+            渲染背景样式预览();
         });
 
         const heatmapColourPicker2 = containerEl.createEl("input", {
@@ -4495,6 +4608,7 @@ class editSettingsTab extends obsidian.PluginSettingTab {
         heatmapColourPicker2.addEventListener("change", async () => {
             this.plugin.settings.bColor2 = heatmapColourPicker2.value;
             await this.plugin.saveSettings();
+            渲染背景样式预览();
         });
         
         const heatmapColourPicker3 = containerEl.createEl("input", {
@@ -4504,6 +4618,7 @@ class editSettingsTab extends obsidian.PluginSettingTab {
         heatmapColourPicker3.addEventListener("change", async () => {
             this.plugin.settings.bColor3 = heatmapColourPicker3.value;
             await this.plugin.saveSettings();
+            渲染背景样式预览();
         });
 
         const heatmapColourPicker4 = containerEl.createEl("input", {
@@ -4513,6 +4628,7 @@ class editSettingsTab extends obsidian.PluginSettingTab {
         heatmapColourPicker4.addEventListener("change", async () => {
             this.plugin.settings.bColor4 = heatmapColourPicker4.value;
             await this.plugin.saveSettings();
+            渲染背景样式预览();
         });
 
         const heatmapColourPicker5 = containerEl.createEl("input", {
@@ -4522,6 +4638,7 @@ class editSettingsTab extends obsidian.PluginSettingTab {
         heatmapColourPicker5.addEventListener("change", async () => {
             this.plugin.settings.bColor5 = heatmapColourPicker5.value;
             await this.plugin.saveSettings();
+            渲染背景样式预览();
         });
 
         /*
